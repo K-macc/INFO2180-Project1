6ef4cf466d0bfd130a2a8181942cdbea93e6cf77
K-macc <mkeshawn107@gmail.com>
2023-11-06 08:59:05 -0500

    Landing Page Complete

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
-/* Add your JavaScript to this file */
+document.addEventListener('DOMContentLoaded', function (create) {
+    const cardlist = document.querySelectorAll(".cards");
+    const subscribe = document.querySelectorAll('button');
+    const msg = document.getElementsByClassName("message");
+    var email = document.getElementById("email");
+
+    cardlist.forEach((card, index) => {
+        card.classList.add('card');
+    })
+
+    subscribe[0].addEventListener('click', function (e) {
+        e.preventDefault();
+        if (email.value == "") {
+            msg[0].textContent = "Please enter a valid email address.";
+        } else {
+            msg[0].textContent = `Thank you! Your email address '${email.value}' has been added to our mailing list!`;
+            
+        }
+
+    })
+
+
+})
